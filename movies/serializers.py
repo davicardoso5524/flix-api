f0819cc8c3fc068a8197d91dcfe23a99c37f5cba
Davi Cardoso <davicardoso524@academico.ufs.br>
2025-06-30 22:38:36 -0300

    Subindo o projeto

---
+++ movies/serializers.py
@@ -0,0 +1,42 @@
+from django.db.models import Avg
+from rest_framework import serializers
+from movies.models import Movie
+from genres.models import Genre
+from actors.models import Actor
+
+class MovieSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    genre = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(),
+    )
+    release_date = serializers.DateField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(),
+        many=True,    
+    )
+    resume = serializers.CharField()
+
+class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        
+        return None
+    
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1900.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo não pode ser maior do que 500 caracteres.')
+        return value
